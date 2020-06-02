5bf84f5c20707676399e4a0a4963f7d829c60f8f
Aayush <aayusshkharel2@gmail.com>
2020-06-03 00:35:31 +1000

    fixed server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app = express()
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(express.static('geniusapi'))
+const app = express();
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/geniusapi/'})
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/geniusapi'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/geniusapi/index.html'));
 });
 
-app.listen(process.env.PORT || 8080)
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
